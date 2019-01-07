c9e6b0819e003a498c88a6f8389d8f037e92988f
sharonbash26 <45743653+sharonbash26@users.noreply.github.com>
2019-01-08 01:14:59 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#include "MySerialServer.h"
+#include "MyTestClientHandler.h"
+#include "alg/StringReverser.h"
+
+int main(int argc, char** argv){
+    try {
+        server_side::Server *server = new server_side::MySerialServer();
+        server_side::Solver<std::string, std::string> *solver = new server_side::StringReverser();
+        server_side::ClientHandler *handler = new server_side::MyTestClientHandler(solver);
+        server->open(8000, handler);
+
+        std::string s;
+        std::cout << "Server is running on port 8000, press 's' then enter to stop\n";
+        std::cin >> s;
+        server->stop();
+        std::cout << "Server stopped.\n";
+    }catch(const char* e){
+        printf("Catched %s...", e);
+        exit(1);
+    }
+    return 0;
+}
